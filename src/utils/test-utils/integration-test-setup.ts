ba5dd189f4c494b99fc46573ab2bcfa702ab969b
ISMAIL ELAYADE <elayadeismail@gmail.com>
2026-03-08 15:46:27 +0000

    chore: remove double extension .ts.ts

--- src/utils/test-utils/integration-test-setup.ts
+++ src/utils/test-utils/integration-test-setup.ts
@@ -1,19 +1,20 @@
-import {exec as execCallback} from 'node:child_process';
-import {promisify} from 'node:util';
-import {rm} from 'node:fs/promises';
-import {beforeEach, afterEach} from 'vitest';
-import {CONFIG} from '../../configuration/index.js';
+import { exec as execCallback } from "node:child_process";
+import { promisify } from "node:util";
+import { rm } from "node:fs/promises";
+import { beforeEach, afterEach } from "vitest";
+import { CONFIG } from "../../configuration/index.js";
 
 const exec = promisify(execCallback);
-const databaseConfig = CONFIG.get('db');
+const databaseConfig = CONFIG.get("db");
 
 beforeEach(async () => {
-	await exec(`pnpm drizzle-kit push --schema=src/db/schema.ts --dialect=sqlite --url=${databaseConfig.url}`);
-	console.log('✅ Database created');
+  await exec(
+    `pnpm drizzle-kit push --schema=src/db/schema.ts --dialect=sqlite --url=${databaseConfig.url}`,
+  );
+  console.log("✅ Database created");
 });
 
 afterEach(async () => {
-	await rm(databaseConfig.url);
-	console.log('✅ Database deleted');
+  await rm(databaseConfig.url);
+  console.log("✅ Database deleted");
 });
-
---
+++ src/utils/test-utils/unit-test-setup.ts
@@ -0,0 +1,12 @@
+import {
+  UNIT_TEST_DB_PREFIX,
+  cleanAllLooseDatabases,
+} from "./database-tools.js";
+
+export async function setup() {
+  await cleanAllLooseDatabases(UNIT_TEST_DB_PREFIX);
+}
+
+export async function teardown() {
+  await cleanAllLooseDatabases(UNIT_TEST_DB_PREFIX);
+}
